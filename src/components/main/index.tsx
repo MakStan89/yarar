6cdc224b472eb065ceb2880697219ed94020f607
MakStan89 <stanislavmakarkinn@gmail.com>
2023-01-27 00:27:29 +0300

    feat(yarar): first commit

---
+++ src/components/main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export const Main = () => {
+  return (
+    <header className={styles.main}>
+      
+    </header>
+  )
+}
+
+export default Main;
